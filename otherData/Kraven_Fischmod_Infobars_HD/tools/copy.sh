a2593e67e27e01bf9684e258281e97c7494ad293
Fischreiher <lukas.bauer5@web.de>
2018-07-24 09:31:39 +0200

    removed MetrixHD - MyMetrixLite can create the links

--- otherData/Kraven_Fischmod_Infobars_HD/tools/copy.sh
+++ otherData/Kraven_Fischmod_Infobars_HD/tools/copy.sh
@@ -5,8 +5,4 @@ rm -rf ~/R/usr/share/enigma2/MetrixHD/skinparts
 rm -rf ~/R/usr/share/enigma2/DMConcinnity-HD/allScreens
 rm -rf ~/R/usr/share/enigma2/DMConcinnity-HD/skinparts
 
-cp -ar ../../../allScreens ~/R/usr/share/enigma2/MetrixHD
-cp -ar ../../../skinparts  ~/R/usr/share/enigma2/MetrixHD
-cp -ar ../../../allScreens ~/R/usr/share/enigma2/DMConcinnity-HD
-cp -ar ../../../skinparts  ~/R/usr/share/enigma2/DMConcinnity-HD
-
+cp -ar ../../../skinparts  ~/R/usr/share/enigma2
