c5e5204a670118709781fa3d3b0cf4784d96f846
Fischreiher <lukas.bauer5@web.de>
2018-07-25 12:29:19 +0200

    renamed icons to icons_HD / trial HD+FHD file for Metrix

--- Universal/Fischreiher_Infobars/otherData/Fischreiher_Infobars_KravenVBlike_HD/tools/ConvertAtileToMetrix.sh
+++ Universal/Fischreiher_Infobars/otherData/Fischreiher_Infobars_KravenVBlike_HD/tools/ConvertAtileToMetrix.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-cat ../../../skinparts/Fischreiher_Infobars_KravenVBlike_HD/Fischreiher_Infobars_KravenVBlike_HD_Atile.xml \
+file_Atile_HD=../../../skinparts/Fischreiher_Infobars_KravenVBlike_HD/Fischreiher_Infobars_KravenVBlike_HD_Atile.xml
+file_Metrix_HD=../../../skinparts/Fischreiher_Infobars_KravenVBlike_HD/Fischreiher_Infobars_KravenVBlike_HD.xml
+
+cat $file_Atile_HD \
 \
 | sed -e "s|\"KravenIBbg\"|\"black\"|g" \
 | sed -e "s|\"KravenIBFont1\"|\"white\"|g" \
@@ -15,16 +18,18 @@ cat ../../../skinparts/Fischreiher_Infobars_KravenVBlike_HD/Fischreiher_Infobars
 | perl -0777 -pe 's|\ *<fonts>.*</fonts>\ *\n||igs' \
 | perl -0777 -pe 's|\ *<parameters>.*</parameters>\ *\n||igs' \
 \
- > ../../../skinparts/Fischreiher_Infobars_KravenVBlike_HD/Fischreiher_Infobars_KravenVBlike_HD.xml
-
+| grep -v "<skin>" \
+| grep -v "</skin>" \
+\
+ > $file_Atile_HD.part_HD
  
-./skin_scale.pl 1280x720 1920x1080 auto \
-../../../skinparts/Fischreiher_Infobars_KravenVBlike_HD/Fischreiher_Infobars_KravenVBlike_HD.xml \
-../../../skinparts/Fischreiher_Infobars_KravenVBlike_HD/Fischreiher_Infobars_KravenVBlike_FHD.xml
+./skin_scale.pl 1280x720 1920x1080 auto $file_Atile_HD.part_HD $file_Atile_HD.part_FHD
 
+sed -r -i 's|screen\s+name=\"([A-Za-z0-9\_]+)\"|screen\ name=\"\1#_FHDscreen\"|g' $file_Atile_HD.part_FHD
+sed -r -i 's|/icons_HD/|/icons_FHD/|g'     $file_Atile_HD.part_FHD
+sed -r -i 's|HD\s+section\s|FHD section|g' $file_Atile_HD.part_FHD
 
-sed -r -i 's|screen\s+name=\"([A-Za-z0-9\_]+)\"|screen\ name=\"\1#_FHDscreen\"|g' \
-../../../skinparts/Fischreiher_Infobars_KravenVBlike_HD/Fischreiher_Infobars_KravenVBlike_FHD.xml
-
-sed -r -i 's|/icons/|/icons_FHD/|g' \
-../../../skinparts/Fischreiher_Infobars_KravenVBlike_HD/Fischreiher_Infobars_KravenVBlike_FHD.xml
+echo "<skin>"                > $file_Metrix_HD
+cat $file_Atile_HD.part_HD  >> $file_Metrix_HD
+cat $file_Atile_HD.part_FHD >> $file_Metrix_HD
+echo "</skin>"              >> $file_Metrix_HD
--- Universal/Fischreiher_Infobars/otherData/Fischreiher_Infobars_KravenVBlike_HD/tools/copy.sh
+++ Universal/Fischreiher_Infobars/otherData/Fischreiher_Infobars_KravenVBlike_HD/tools/copy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-rm -rf ~/R/usr/share/enigma2/MetrixHD/allScreens 
-rm -rf ~/R/usr/share/enigma2/MetrixHD/skinparts
-rm -rf ~/R/usr/share/enigma2/DMConcinnity-HD/allScreens
-rm -rf ~/R/usr/share/enigma2/DMConcinnity-HD/skinparts
+#rm -rf ~/R/usr/share/enigma2/MetrixHD/allScreens 
+#rm -rf ~/R/usr/share/enigma2/MetrixHD/skinparts
+#rm -rf ~/R/usr/share/enigma2/DMConcinnity-HD/allScreens
+#rm -rf ~/R/usr/share/enigma2/DMConcinnity-HD/skinparts
 rm -rf ~/R/usr/share/enigma2/skinparts
 
 cp -ar ../../../skinparts  ~/R/usr/share/enigma2
